35cb62121d198cb2afd20d80d7fb0c15b5a9b0d7
sirJohn72 <sirJohn72@outlook.es>
2023-04-15 17:17:11 -0600

    index main page fixed

--- pages/version3d/index.tsx
+++ pages/version3d/index.tsx
@@ -1,19 +1,7 @@
 import SceneComponente from "@/components/Three/Main/SceneComponente";
 import React from "react";
-import { useLoaderScene } from "../../components/Three/Store/ThreeState";
 
 export default function index() {
-  const { setMusicLoaded, setSceneLoaded } = useLoaderScene(
-    (state) => state
-  );
-
-  React.useEffect(() => {
-    return () => {
-      setMusicLoaded(false);
-      setMusicLoaded(false);
-    };
-  }, []);
-
   return (
     <div>
       <SceneComponente />
