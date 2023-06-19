ac4e0177d57df6360215eef668284453a811b1c6
sirJohn72 <sirJohn72@outlook.es>
2023-06-19 01:01:32 -0600

    fix(Plane physics): Plane physics fixed with new dimensions

--- components/Three/Canvas/Physics/Utils/PlaneSecctionPhysics.tsx
+++ components/Three/Canvas/Physics/Utils/PlaneSecctionPhysics.tsx
@@ -4,18 +4,14 @@ import { useBox } from "@react-three/cannon";
 export default function PlaneSecctionPhysics() {
   const [ref] = useBox<any>(() => ({
     type: "Static",
-    position: [40.7552, -0.56, 1.45813],
-    args: [113, 1, 34],
+    position: [36.25, -0.56, 1.45813],
+    args: [122, 1, 34],
   }));
 
   return (
     <mesh ref={ref} position={[0, 0, 0]} name='floor'>
-      <boxGeometry args={[113, 1, 34]} />
-      <meshBasicMaterial
-        color={"#ff0000"}
-        opacity={0}
-        transparent
-      />
+      <boxGeometry args={[120, 1, 34]} />
+      <meshBasicMaterial color={"#ff0000"} opacity={0} />
     </mesh>
   );
 }
